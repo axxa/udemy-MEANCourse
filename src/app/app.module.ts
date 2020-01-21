b0b371d74269e32e4fdecf849b6ddae6618dc64d
Alvaro <asan123456@gmail.com>
2020-01-21 22:00:00 +0100

    feat: first ang material setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/posts-create/post-create.component';
@@ -8,11 +10,15 @@ import { PostCreateComponent } from './posts/posts-create/post-create.component'
 @NgModule({
   declarations: [
     AppComponent,
-	PostCreateComponent
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
