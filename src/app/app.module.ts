9b4c874a73c53ca758586e4846e5aba179a006af
Alvaro <asan123456@gmail.com>
2020-01-23 21:08:43 +0100

    feat: getting posts from post-create to post-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/posts-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
-
+ 
 @NgModule({
   declarations: [
     AppComponent,
