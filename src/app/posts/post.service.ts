bc114eae59c48341cd3797490a518d72258f171b
Alvaro <asan123456@gmail.com>
2020-01-29 20:54:46 +0100

    feat: transforming response data

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -12,9 +13,21 @@ export class PostService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http
+      .get<{message: string, posts: any}>(
+        'http://localhost:3000/api/posts'
+      )
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -26,7 +39,7 @@ export class PostService {
   addPost(title: string, content: string ) {
     const post: Post = { id: null, title: title, content: content};
     this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
-    .subscribe((responseData) =>{
+    .subscribe((responseData) => {
       console.log(responseData.message);
       this.posts.push(post);
       this.postsUpdated.next([...this.posts]);
